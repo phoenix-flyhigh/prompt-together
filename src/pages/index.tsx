86f505387ab335660324c4d8c504b19bb45c14fd
phoenix-flyhigh <j.akshaya07@gmail.com>
2025-03-21 16:59:11 +0530

    initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="w-full h-screen">
+      <main className="flex justify-center items-center h-screen">
+        Hello, World!
+      </main>
+    </div>
+  );
+}
